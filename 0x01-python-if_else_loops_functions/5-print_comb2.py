4aeb80ada55d32a4f1d93f3c5ecf85005ef39a3c
mmbouchoucha <mehdiboch@gmail.com>
2019-12-10 22:40:29 +0000

    up04

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-print('{:02d}'.format(i), end=', ')
-print('{:d}'.format(99))
+for num in range(0, 100):
+        if num != 99:
+                print("{:02d}, ".format(num), end='')
+        else:
+                print("{:02d}".format(num))
